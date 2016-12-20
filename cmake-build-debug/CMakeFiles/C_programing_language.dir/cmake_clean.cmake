ee46d59af9a8f5fe8524da22936b6db704c07503
xiaonengmiao <1123913594@qq.com>
2016-12-20 18:59:39 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/C_programing_language.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_programing_language.dir/main.cpp.o"
+  "C_programing_language.pdb"
+  "C_programing_language"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_programing_language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/C_programing_language.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_programing_language.
+# This may be replaced when dependencies are built.
